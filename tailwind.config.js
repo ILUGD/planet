9cc5947351e5798f51e8baac18511103d8e8f68d
prashant.srivastava <prashant.srivastava@99acres.com>
2021-07-17 17:00:59 +0530

    :lipstick: UI Revamp

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 const colors = require("tailwindcss/colors");
+const defaultTailwind = require('tailwindcss/defaultTheme');
 
 module.exports = {
   purge: false,
@@ -13,20 +14,25 @@ module.exports = {
         'light-wool': "url('/assets/light-wool.png')"
       })
     },
-    // colors: {
-    //   black: gruvbox.dark.hard,
-    //   white: colors.white,
-    //   paper: gruvbox.light[0],
-    //   primary: gruvbox.dark[0],
-    //   secondary: gruvbox.red.neutral,
-    //   ...gruvbox,
-    // },
+    colors: {
+      ...colors,
+      apnaGreen: "#04D98B",
+      dateGreen: "#038C5A",
+      apniHeading: "#014029",
+      neechuBG: "#3B3C40",
+      neechuText: "#f2f2f2",
+      ivory: "#FFFFF0",
+    },
     fontFamily: {
       sans: [
         "system-ui",
         "sans-serif",
       ],
     },
+    boxShadow: {
+      ...defaultTailwind.boxShadow,
+      apnaShadow: "0 25px 50px -12px rgba(1, 64, 41, 0.25)"
+    }
   },
   variants: {},
   plugins: [],
